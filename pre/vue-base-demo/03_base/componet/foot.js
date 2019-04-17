353717fddcd5bcf8ff28344d47c0937b7b59cd28
damu <damu@example.com>
2019-04-17 08:29:48 +0800

    add pre

---
+++ pre/vue-base-demo/03_base/componet/foot.js
@@ -0,0 +1,11 @@
+(function (w) {
+    var footDown={
+        template:`<header slot="up">
+                        <p>我是footer</p>
+                        <p>我是footer</p>
+                        <p>我是footer</p>
+                        <p>我是footer</p>
+                    </header>`
+    }
+    w.footDown = footDown
+})(window)
---
+++ pre/vue-base-demo/03_base/componet/head.js
@@ -0,0 +1,11 @@
+(function (w) {
+    var headUp={
+        template:`<header slot="up">
+                        <p>我是header</p>
+                        <p>我是header</p>
+                        <p>我是header</p>
+                        <p>我是header</p>
+                    </header>`
+    }
+    w.headUp = headUp
+})(window)
