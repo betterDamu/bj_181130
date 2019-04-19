847587be8cb8cbe70f6e75e7ea29ed9a50513bfa
damu <damu@example.com>
2019-04-19 14:58:30 +0800

    code for day04-4

--- work/todolist/README.md
+++ work/todolist/README.md
@@ -8,7 +8,18 @@
   3. vue list 查看对应的模板类型
   4. vue init webpack 项目名
 
-# vue 模板
+## 开发环境的启动流程
+   webpack.dev.conf.js
+   webpack.base.conf.js
+     entry :  main.js : webpack整个的入口文件
+     output:  app.js
+   
+   HtmlWebpackPlugin
+    指定了项目的模板!!!  ---> el!!! 
+    app.js注入到index.html中
+    
+   所有的静态资源都应该放在指定的 static 目录下
+## vue 模板
 <template>
 #[[$END$]]#
 </template>
--- work/todolist/src/components/item.vue
+++ work/todolist/src/components/item.vue
@@ -1,23 +1,45 @@
 <template>
-  <li>
+  <li @mouseenter="isShow = !isShow" @mouseleave="isShow = !isShow" :class="{highlight:isShow}">
+  <!--<li @mouseenter="handleMouse('enter')" @mouseleave="handleMouse('leave')" :class="{highlight:highlight}">-->
     <label>
       <input type="checkbox" v-model="todo.completed" />
       <span>{{todo.text}}</span>
     </label>
-    <button class="btn btn-danger" style="display:none">删除</button>
+    <button class="btn btn-danger" v-show="isShow">删除</button>
   </li>
 </template>
 
 <script type="text/ecmascript-6">
     export default {
         name: "todo-item",
+        data(){
+          return {
+            // highlight:false,
+            isShow:false
+          }
+        },
         props:{
           todo:Object
-        }
+        },
+        /*methods:{
+          handleMouse(flag){
+             if (flag === "enter"){
+                this.highlight=true;
+                this.isShow = true;
+             }else if(flag === "leave"){
+               this.highlight=false;
+               this.isShow = false;
+             }
+          }
+        }*/
     }
 </script>
 
 <style  scoped>
+  li.highlight{
+    background: pink;
+  }
+
   li {
     list-style: none;
     height: 36px;
@@ -40,7 +62,6 @@
 
   li button {
     float: right;
-    display: none;
     margin-top: 3px;
   }
 
