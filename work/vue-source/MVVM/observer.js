0e61c83d57752abdd0747d864c581b5a78dfeb01
damu <damu@example.com>
2019-04-26 10:58:55 +0800

    code for day09-2

---
+++ work/vue-source/MVVM/observer.js
@@ -0,0 +1,89 @@
+function Observer(data) {
+    this.data = data;
+    this.walk(data);
+}
+
+Observer.prototype = {
+    walk: function(data) {
+        var me = this;
+        Object.keys(data).forEach(function(key) {
+            me.convert(key, data[key]);
+        });
+    },
+    convert: function(key, val) {
+        this.defineReactive(this.data, key, val);
+    },
+    defineReactive: function(data, key, val) {
+        //每一个data中的属性（包括深层次属性都一个dep闭包）
+        var dep = new Dep();
+
+
+        var childObj = observe(val);
+
+
+        //真正的数据劫持的代码
+        Object.defineProperty(data, key, {
+            enumerable: true, // 可枚举
+            configurable: false, // 不能再define
+            get: function() {
+                if (Dep.target) {
+                	//使用代码构建dep与watcher的多对多关系
+                    dep.depend();
+                }
+                return val;
+            },
+            set: function(newVal) {
+                if (newVal === val) {
+                    return;
+                }
+                val = newVal;
+                // 新的值是object的话，进行监听
+                childObj = observe(newVal);
+                // 通知订阅者
+                dep.notify();
+            }
+        });
+    }
+};
+
+//value是配置对象的data属性
+//vm
+function observe(value, vm) {
+    if (!value || typeof value !== 'object') {
+        return;
+    }
+    return new Observer(value);
+};
+
+
+var uid = 0;
+
+function Dep() {
+    this.id = uid++;
+    this.subs = [];
+}
+
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+
+    depend: function() {
+        Dep.target.addDep(this);
+    },
+
+    removeSub: function(sub) {
+        var index = this.subs.indexOf(sub);
+        if (index != -1) {
+            this.subs.splice(index, 1);
+        }
+    },
+
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
+};
+
+Dep.target = null;
---
+++ work/vue-source/MVVM/watcher.js
@@ -0,0 +1,44 @@
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    this.depIds = {};
+    this.value = this.get();
+}
+
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.get();
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    addDep: function(dep) {
+        if (!this.depIds.hasOwnProperty(dep.id)) {
+            dep.addSub(this);
+            this.depIds[dep.id] = dep;
+        }
+    },
+    get: function() {
+        Dep.target = this;
+        var value = this.getVMVal();
+        Dep.target = null;
+        return value;
+    },
+
+    //构建dep和watcher的关系
+    getVMVal: function() {
+    	// damu.wife
+        var exp = this.exp.split('.');
+        var val = this.vm._data;
+        exp.forEach(function(k) {
+            val = val[k];
+        });
+        return val;
+    }
+};
