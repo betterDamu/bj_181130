38581256736799d12890901b60cc81ffb53d52f4
damu <damu@example.com>
2019-04-27 13:40:33 +0800

    code for day10-3

---
+++ work/node_package/hello.js
@@ -0,0 +1,26 @@
+var colors = require('colors');
+console.log(colors)
+// 1. node内建包
+/*
+    2. 自己文件的路径
+        不会主动加 ./ ../ /
+*/
+/*
+    3. 第三方包
+         module.paths[0]
+            ---> 找包名
+             ---> 找包底下的package.json
+               ---> package.json  main字段所指向的文件
+                 ---> 如果main字段不存在 直接找包名底下的index.js  index.json
+ */
+
+
+/*    [ 'C:\\Users\\alienware\\Desktop\\北京_前端_181130\\bj_181130\\work\\node_package\\node_modules',
+    'C:\\Users\\alienware\\Desktop\\北京_前端_181130\\bj_181130\\work\\node_modules',
+    'C:\\Users\\alienware\\Desktop\\北京_前端_181130\\bj_181130\\node_modules',
+    'C:\\Users\\alienware\\Desktop\\北京_前端_181130\\node_modules',
+    'C:\\Users\\alienware\\Desktop\\node_modules',
+    'C:\\Users\\alienware\\node_modules',
+    'C:\\Users\\node_modules',
+    'C:\\node_modules' ]*/
+
---
+++ work/vue-cli-demo/cli-demo/src/main.js
@@ -0,0 +1,6 @@
+import  x from "lodash";
+console.dir(x)
+
+
+import  damu from "damu";
+console.log(damu)
