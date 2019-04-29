92621bd30a77276f92b455eb113c9ae70cac7671
damu <damu@example.com>
2019-04-29 09:48:11 +0800

    vue项目布局编写-搭建脚手架环境

---
+++ project/atguigu-elm/README.md
@@ -0,0 +1,5 @@
+# 怎么做字体图标
+# 怎么引入样式
+    link
+    import
+    @import
---
+++ project/atguigu-elm/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <span class="icon-youtube2"></span>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style scoped>
+</style>
